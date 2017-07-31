34f431b9e43662e59625e4b5e676155d0a9d25c8
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-07-31 17:28:57 +0300

    add es6 decorators to babel, create base layout for TodoView component

--- src/ObservableTodoStore.js
+++ src/ObservableTodoStore.js
@@ -1,35 +1,51 @@
 import shortid from 'shortid';
-import mobx, { observable } from 'mobx';
+import mobx, { observable, computed } from 'mobx';
 
 /**
- * TodoStore - provides core methods to interact with
- * todos (such as creating, editing, removing, etc.)
+ * ObservableTodoStore - provides core methods to interact with
+ * tasks (such as creating, editing, removing, etc.)
  *
  * Example:
- *      todoStore.addTodo("Buy a bottle of milk in Wallmart after work")
+ *      observableTodoStore.addTodo("Buy a bottle of milk in Wallmart after work")
  */
 class ObservableTodoStore {
     @observable todos = [];
     @observable pendingRequests = 0;
     
     constructor() {
+        // shows report about current tasks in console after this.todos changing
+        // automatically
         mobx.autorun(() => console.log(this.report));
     }
 
+    /**
+     * Returns count of all completed tasks
+     * @returns {Number}
+     */
     @computed get completedTodosCount() {
         return this.todos.filter(
             todo => todo.completed === true
         ).length;
     }
 
+    /**
+     * Returns common report about current tasks
+     * If there are some tasks - shows statistics and next task.
+     * Otherwise, shows a message with instructions "How to Start".
+     * @returns {string}
+     */
     @computed get report() {
         if (this.todos.length === 0)
-            return "<none>";
+            return "No tasks yet. You can create a new task using the button below.";
 
         return `Next todo: "${this.todos[0].task}". ` +
             `Progress: ${this.completedTodosCount}/${this.todos.length}`;
     }
 
+    /**
+     * Adds a new task to the list
+     * @param text  Message of the task
+     */
     addTodo(text) {
         this.todos.push({
             id: shortid.generate(),
@@ -40,4 +56,4 @@ class ObservableTodoStore {
     }
 }
 
-export default TodoStore;
+export default ObservableTodoStore;
--- src/TodoView.js
+++ src/TodoView.js
@@ -2,9 +2,39 @@ import React, { Component } from 'react';
 
 class TodoView extends Component {
     render() {
+        const todo = this.props.todo;
         return (
-            <div>TodoView Component</div>
+            <li onDoubleClick={ this.onRename }>
+                <input
+                    type='checkbox'
+                    checked={ todo.completed }
+                    onChange={ this.onToggleCompleted }
+                />
+                { todo.text }
+                { todo.assignee
+                    ? <small>{ todo.assignee.name }</small>
+                    : null
+                }
+            </li>
         )
+    }
+
+    /**
+     * Change a completed status of the task
+     * Sets status as completed if not completed yet.
+     * Otherwise, sets a status as not completed.
+     */
+    onToggleCompleted = () => {
+        const todo = this.props.todo;
+        todo.completed = !todo.completed;
+    }
+
+    /**
+     * Calls a prompt for changing the task name
+     */
+    onRename = () => {
+        const todo = this.props.todo;
+        todo.text = prompt('Task name', todo.text) || "";
     }
 }
 
