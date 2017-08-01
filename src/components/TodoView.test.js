fd268ef47e67a4ffd75818331db4fa9d2bdef32d
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-08-01 16:49:08 +0300

    add TodoView snapshot testing

--- src/components/TodoView.test.js
+++ src/components/TodoView.test.js
@@ -1,5 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoView from './TodoView';
+import renderer from 'react-test-renderer';
 
-it("")
+it("renders correctly for not completed tasks", () => {
+
+    const todo = {
+        text: "Reminder to buy milk after work",
+        completed: false
+    };
+
+    const tree = renderer.create(
+        <TodoView todo={todo}/>
+    );
+
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders correctly for completed tasks", () => {
+    const todo = {
+        text: "Reminder to buy milk after work",
+        completed: true
+    };
+
+    const tree = renderer.create(
+        <TodoView todo={todo}/>
+    );
+
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders correctly for tasks with assignee", () => {
+    const todo = {
+        text: "Reminder to buy milk after work",
+        completed: true,
+        assignee: {
+            name: "John Doe"
+        }
+    };
+
+    const tree = renderer.create(
+        <TodoView todo={todo}/>
+    );
+
+    expect(tree).toMatchSnapshot();
+});
